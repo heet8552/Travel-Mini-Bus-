c6d5715df217e28e204fadeba3c0adb1fc285172
heet8552 <85003803+heet8552@users.noreply.github.com>
2024-02-15 13:15:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal Hire Mee Taxi</title>
+    <link rel="stylesheet" href="landingpage.css">
+    <script src="https://kit.fontawesome.com/db6a4e3294.js" crossorigin="anonymous"></script>
+   
+    
+ 
+</head>
+<body>
+
+
+<!-- <h1>Shree Ganeshay Namah </h1> -->
+
+
+
+<section class="header">
+    <div class="nav-bar">
+        <div class="menu-logo-header-class">
+
+
+
+
+        <img src=".././taxi/images/logo (1) (1).png" alt="header-logo"> 
+        <!-- <img src=".././taxi/images/icons8-hamburger-100.png" alt=""> -->
+        <!-- <input class="menu-btn" type="checkbox" id="menu-btn" /> -->
+        <input type="checkbox" id="click">
+<label for="click" class="menu-button">
+
+<i class="fa-solid fa-bars"></i>
+</label>
+
+
+        <div class="menu-bar">
+    <a class="menu-links" href="#Help">Help</a>
+    <a class="menu-links" href="#" id="popularDestinationLink">Popular Destination</a>
+    <a class="menu-links" href="#RegisterYourService">Register Your Services</a>
+    <a class="menu-links" href="#Business">Business</a>
+    <a class="menu-links" href="#TrackBooking">Track Booking</a>
+    <a class="login-button-header" href="#Login">Login</a>
+    <a class="login-button-header" href="#Sign-Up">Sign Up</a>
+
+    <div class="menu-bar-button">
+     
+        <a class="login-button" href="#Login">Login</a>
+    <a class="login-button" href="#Sign-Up">Sign Up</a>
+
+    </div>
+</div>
+
+
+          
+
+        </div>
+
+    </div>
+
+</section>
+
+</body>
+</html>
